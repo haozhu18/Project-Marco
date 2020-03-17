8c9b1a0269458fb84bee2073b75566402da63e71
haozhu <haozhu@uchicago.edu>
2020-03-16 23:24:57 -0500

    moving round

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -128,3 +128,11 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=True, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
